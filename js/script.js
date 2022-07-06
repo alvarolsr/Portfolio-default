55c9491af6c46bca8b2d8c229f38038a70a609dd
Alvaro Salazar <alvarolsr@gmail.com>
2022-07-06 08:32:23 -0400

    Reordered some preperties
    Reordered some style preoperties, simplified some selectors, added new class to main tag so it smoothly changes padding when menu button is clicked on mobile screen size.

--- js/script.js
+++ js/script.js
@@ -9,11 +9,13 @@ let testMenu = document.getElementById("test-menu");
 
 toggle_button.onclick = function () {
   if (toggle_button.classList.contains("fa-bars")) {
-    mainListDiv.classList.toggle("show_list");
+    mainContainer.classList.toggle("mobileContainer");
+    mainListDiv.classList.toggle("show-list");
     toggle_button.classList.add("fa-times");
     toggle_button.classList.remove("fa-bars");
   } else {
-    mainListDiv.classList.remove("show_list");
+    mainContainer.classList.remove("mobileContainer");
+    mainListDiv.classList.remove("show-list");
     toggle_button.classList.add("fa-bars");
     toggle_button.classList.remove("fa-times");
   }
